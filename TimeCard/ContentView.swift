4e2bf83777f8e416361a7101384d4892b29af78e
Kentaro <kentaro@beah.jp>
2021-07-30 02:05:25 +0900

    Initial Commit

---
+++ TimeCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeCard
+//
+//  Created by Kentaro Abe on 2021/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
